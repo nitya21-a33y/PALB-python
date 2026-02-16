b6a4afbd07390abcced7ef0f91a682b7b4442b7e
nitya21-a33y <nityasharan21@gmail.com>
2026-02-16 09:45:47 +0530

    Update WEEK3.py

--- WEEK3.py
+++ WEEK3.py
@@ -30,4 +30,51 @@ class Solution:
 
 
 
+#Given an array and a range a, b. The task is to partition the array around the range such that the array is divided into three parts.
+#1) All elements smaller than a come first.
+#2) All elements in range a to b come next.
+#3) All elements greater than b appear in the end.
+#The individual elements of three sets can appear in any order. You are required to return the modified array.
+class Solution:
+	def threeWayPartition(self, arr, a, b):
+	    low = 0
+        mid = 0
+        high = len(arr) - 1
+        while mid <= high:
+            if arr[mid] < a:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif a <= arr[mid] <= b:
+                mid += 1
+            else:  
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+        return arr
+
+
+
+
+#Given an array arr and a number k. One can apply a swap operation on the array any number of times, i.e choose any two index i and j (i < j) and swap arr[i] , arr[j] . Find the minimum number of swaps required to bring all the numbers less than or equal to k together, i.e. make them a contiguous subarray.
+
+class Solution:
+    def minSwap (self,arr, k) : 
+        n = len(arr)
+        good = sum(1 for x in arr if x <= k)
+        if good == 0:
+            return 0
+        bad = sum(1 for x in arr[:good] if x > k)
+        ans = bad
+        for i in range(good, n):
+            if arr[i] > k:
+                bad += 1
+            if arr[i - good] > k:
+                bad -= 1
+            ans = min(ans, bad)
+        return ans
+        
+
+
+
+
 #

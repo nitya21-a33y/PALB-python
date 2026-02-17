6c3e2f74e6037b8eacf66f0586efa10e8c9d5c88
nitya21-a33y <nityasharan21@gmail.com>
2026-02-17 06:44:01 +0530

    Update WEEK4.py

--- WEEK4.py
+++ WEEK4.py
@@ -109,7 +109,242 @@ class Solution:
         return [1] + digits
 
 
+#Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m = len(matrix)
+        n = len(matrix[0])
+        first_row_zero = False
+        first_col_zero = False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+        for j in range(n):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if first_col_zero:
+            for i in range(m):
+                matrix[i][0] = 0
+        if first_row_zero:
+            for j in range(n):
+                matrix[0][j] = 0
 
-#
 
- 
+
+
+#You are given an m x n integer matrix matrix with the following two properties:
+#Each row is sorted in non-decreasing order.
+#The first integer of each row is greater than the last integer of the previous row.
+#Given an integer target, return true if target is in matrix or false otherwise.
+class Solution:
+    def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
+        if not mat or not mat[0]:
+            return False
+        n = len(mat)
+        m = len(mat[0])
+        left = 0
+        right = n * m - 1
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // m
+            col = mid % m
+            mid_val = mat[row][col]
+
+            if mid_val == target:
+                return True
+            elif mid_val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+
+
+
+#Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else: 
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+
+
+
+
+#Given an integer array nums of unique elements, return all possible subsets (the power set).
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(index, curr):
+            if index == len(nums):
+                result.append(curr[:])
+                return
+            backtrack(index + 1, curr)
+            curr.append(nums[index])
+            backtrack(index + 1, curr)
+            curr.pop()
+
+        backtrack(0, [])
+        return result
+
+
+
+
+#Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+#The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        
+        def dfs(i, j, k):
+            if k == len(word):
+                return True
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
+                return False
+
+            temp = board[i][j]
+            board[i][j] = "#"   
+
+            found = (
+                dfs(i+1, j, k+1) or
+                dfs(i-1, j, k+1) or
+                dfs(i, j+1, k+1) or
+                dfs(i, j-1, k+1)
+            )
+
+            board[i][j] = temp   
+            return found
+
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0):
+                    return True
+
+        return False
+
+
+
+
+#Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+#0 <= a, b, c, d < n
+#a, b, c, and d are distinct.
+#nums[a] + nums[b] + nums[c] + nums[d] == target
+#You may return the answer in any order.
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        left += 1
+                        right -= 1
+
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return res
+
+
+
+
+
+#There is an integer array nums sorted in ascending order (with distinct values).
+#Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+#Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+
+
+
+#Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value. If target is not found in the array, return [-1, -1].
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findFirst():
+            left, right = 0, len(nums) - 1
+            pos = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                if mid < len(nums) and nums[mid] == target:
+                    pos = mid
+            return pos
+        def findLast():
+            left, right = 0, len(nums) - 1
+            pos = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                if mid < len(nums) and nums[mid] == target:
+                    pos = mid
+            return pos
+        return [findFirst(), findLast()]
+        
